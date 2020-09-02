c276bdc25b634782cafb6a8e8cc4b6195c1682d3
rakeen-zz <rakeen@sina.com>
2020-09-02 19:19:15 +0800

    Update CAIDDGenerator.m
    更新getRAM的实现方式：
    原方法和新方法同时在一个应用中使用时，原方法返回值不稳定。

--- CAIDDGenerator.m
+++ CAIDDGenerator.m
@@ -242,11 +242,9 @@ static bool check_nsstring(NSString *arg) {
 
 //device RAM
 +(NSString *)getRAM {
-    size_t size = sizeof(long);
-    unsigned long results;
-    int mib[2] = {CTL_HW, HW_PHYSMEM};
-    sysctl(mib, 2, &results, &size, NULL, 0);
-    return [NSString stringWithFormat:@"%lu",results];
+    unsigned long long physicalMemory = [[NSProcessInfo processInfo] physicalMemory];
+    NSString *strResult = [NSString stringWithFormat:@"%llu",physicalMemory];
+    return [strResult copy];
 }
 
 //country coude
